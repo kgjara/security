50876d209ddd06f83ed27c7ffb1004dc83ebc9c6
Kenny Jara <84399723+kgjara@users.noreply.github.com>
2024-07-24 00:45:48 -0500

    cambios

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+/* 1. Importe el módulo crypto */
+let crypto = require('crypto');
+
 /* 1. Cargue los modelos de acuerdo con la configuración de la conexión */
 const sequelize = require('../models/index.js').sequelize;
 var initModels = require("../models/init-models");
 var models = initModels( sequelize );  
 
-/* 1. Importe el módulo crypto */
-let crypto = require('crypto');
-
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  //res.send('respond with a resource');
-  
-   /* 3. Uso del método findAll */
-   let usersCollection = await models.users.findAll({ })
+  /* 3. Uso del método findAll */
+  let usersCollection = await models.users.findAll({ 
 
-   /* 4. Paso de parámetros a la vista */
-   res.render('crud', { title: 'CRUD with users', usersArray: usersCollection });
+    /* 3.1. Including everything */
+    include: { all: true, nested: true },
+       
+    /* 3.2. Raw Queries */
+    raw: true,
+    nest: true,
+       
+       
+    })
+
+  let rolesCollection = await models.roles.findAll({ })
+
+  /* 4. Paso de parámetros a la vista */
+       res.render('crud', { title: 'CRUD of users', usersArray: usersCollection, rolesArray: rolesCollection   });
 });
 
-/* POST user. */
+
+
+ /* POST user. */
  /* 2. Cree el callback asíncrono que responda al método POST */
  router.post('/', async (req, res) => {
 
@@ -36,6 +48,9 @@ router.get('/', async function(req, res, next) {
 
     /* 5. Guarde el registro mediante el método create */
     let user = await models.users.create({ name: name, password: passwordHash })
+
+    /* 5.1. Utilice el model.user_roles para crear la relación ( user.iduser , idrole) */
+    await models.users_roles.create({ users_iduser: user.iduser, roles_idrole: idrole })
 
     /* 6. Redireccione a la ruta con la vista principal '/users' */
     res.redirect('/users')
